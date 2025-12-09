2e13c4fd7355799b5f47b63e3a83a9e5344b6f7e
Miz <sufiworkswith@gmail.com>
2025-12-09 12:53:08 +0500

    Add subscription-related attributes and relationships to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,18 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'subscription_plan_id',
+        'subscription_started_at',
+        'subscription_ends_at',
+        'stripe_customer_id',
+        'stripe_subscription_id'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'subscription_started_at' => 'datetime',
+        'subscription_ends_at' => 'datetime',
     ];
 
     /**
@@ -44,5 +56,30 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relationship
+    public function subscriptionPlan()
+    {
+        return $this->belongsTo(SubscriptionPlan::class);
+    }
+
+    public function websites()
+    {
+        return $this->hasMany(Website::class);
+    }
+
+    public function hasActiveSubscription()
+    {
+        return $this->subscription_plan_id &&
+            (!$this->subscription_ends_at || $this->subscription_ends_at->isFuture());
+    }
+
+    public function canAddWebsite()
+    {
+        if (!$this->subscriptionPlan) {
+            return false;
+        }
+        return $this->websites()->count() < $this->subscriptionPlan->max_websites;
     }
 }
